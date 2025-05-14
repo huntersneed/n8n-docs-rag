feeecc0ffbb841582f52b41d72b5c27e9c47f948
“Hunter <hello@huntersneed.com>
2025-05-14 08:34:10 -0400

    removed cookies

--- get_docs.py
+++ get_docs.py
@@ -3,18 +3,7 @@ import json
 from bs4 import BeautifulSoup
 
 def fetch_docs():
-    cookies = {
-        'n8n_tracking_id': '3hs3ml0yu4lmamd0y16',
-        '_ga': 'GA1.1.862162559.1747131569',
-        '_gcl_au': '1.1.196649388.1747216050',
-        '__sec__cid': '5877e26c078d640',
-        '__sec__token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDczMDI1NTQsImlkIjoiNTg3N2UyNmMwNzhkNjQwIn0.qM4aJ__2G53hhV0qHpA3waJzQKADCJlX_j1GAQt4xZs',
-        '__sec__fid': 'b2ebf8cf8fc59f01d82188663b090d70',
-        '__sec_crid': '107d8018c85d660b4bfda4c3db5ce061ca5da15fbc92bae4e1bc10b40e608709',
-        '__sec_tid': 'glrjwh',
-        '_ga_0SC4FF2FH9': 'GS2.1.s1747216047$o3$g1$t1747216161$j52$l0$h0',
-    }
-    resp = requests.get('https://docs.n8n.io/search/search_index.json', cookies=cookies)
+    resp = requests.get('https://docs.n8n.io/search/search_index.json')
     resp.raise_for_status()
     data = resp.json()
     docs = []
